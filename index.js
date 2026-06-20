b7895d9f415a04d14cdce7ed6b0cc9a2ed96ad27
devangkawde19 <devangkawde19@gmail.com>
2026-06-20 17:43:37 +0530

    add actions

---
+++ index.js
@@ -0,0 +1,9 @@
+hireme=documentquerySelector("#hireme");
+view=documentquerySelector("#view");
+hireme.addEventListener("click",function(){
+    open("#about")
+});
+
+view.addEventListener("click",function(){
+    open("#projects")
+});   
